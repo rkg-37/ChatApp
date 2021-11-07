7ea88df56c7a59eebaebcf0a342b0652e026cbb6
rkg-37 <gupta2.rajkamal2@gmail.com>
2021-11-07 22:17:55 +0530

    working done on 7/11/2021

---
+++ app.js
@@ -0,0 +1,10 @@
+const messageType = { LEFT: "left", RIGHT: "RIGHT", LOGIN: "login" };
+
+// chat
+const chatWindow = document.getElementById("chat");
+const messageList = document.getElementById("messageList");
+const messageInput = document.getElementById("messageInput");
+const sendBtn = document.getElementById("sendBtn");
+//login
+
+const message = []; // date , message , author,type
